c36f64eac2bb71dc82812f947d1e7065abe1022f
mrxzhli-ship-it <mrxzhli@gmail.com>
2026-03-30 18:21:11 +0800

    Update misc.lua

--- src/tabs/misc.lua
+++ src/tabs/misc.lua
@@ -1,1 +1,11 @@
-
+return function(tab)
+    tab:CreateDropdown({
+        Name = "Choose Option",
+        Options = {"Option 1", "Option 2", "Option 3"},
+        CurrentOption = "Option 1",
+        Flag = "MiscOption",
+        Callback = function(option)
+            print("Selected:", option)
+        end
+    })
+end
