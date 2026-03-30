3b66754444286eea4f46b84a057cdbab9240fe7b
mrxzhli-ship-it <mrxzhli@gmail.com>
2026-03-30 18:20:51 +0800

    Update farm.lua

--- src/tabs/farm.lua
+++ src/tabs/farm.lua
@@ -1,1 +1,14 @@
-
+return function(tab)
+    tab:CreateSlider({
+        Name = "Farm Speed",
+        Min = 1,
+        Max = 100,
+        Increment = 1,
+        Suffix = "x",
+        CurrentValue = 10,
+        Flag = "FarmSpeed",
+        Callback = function(value)
+            print("Farm Speed:", value)
+        end
+    })
+end
