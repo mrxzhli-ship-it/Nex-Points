04b0d5082bd3988eb22a456c3317f93638fa9e9e
mrxzhli-ship-it <mrxzhli@gmail.com>
2026-03-30 18:38:02 +0800

    Update player.lua

--- src/tabs/player.lua
+++ src/tabs/player.lua
@@ -1,17 +1,43 @@
+-- tabs/player.lua
 return function(tab)
-    tab:CreateToggle({
-        Name = "Godmode",
-        CurrentValue = false,
-        Flag = "GodmodeToggle",
+    -- WalkSpeed slider
+    tab:CreateSlider({
+        Name = "WalkSpeed",
+        Min = 16,
+        Max = 100,
+        Increment = 1,
+        CurrentValue = 16,
         Callback = function(value)
-            print("Godmode:", value)
+            local humanoid = game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
+            if humanoid then
+                humanoid.WalkSpeed = value
+            end
         end
     })
 
+    -- JumpPower slider
+    tab:CreateSlider({
+        Name = "JumpPower",
+        Min = 50,
+        Max = 200,
+        Increment = 5,
+        CurrentValue = 50,
+        Callback = function(value)
+            local humanoid = game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
+            if humanoid then
+                humanoid.JumpPower = value
+            end
+        end
+    })
+
+    -- Teleport button (example)
     tab:CreateButton({
-        Name = "Teleport (Example)",
+        Name = "Teleport to Spawn",
         Callback = function()
-            print("Teleport triggered")
+            local char = game.Players.LocalPlayer.Character
+            if char and char:FindFirstChild("HumanoidRootPart") then
+                char.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(0,5,0)) -- example spawn coords
+            end
         end
     })
 end
