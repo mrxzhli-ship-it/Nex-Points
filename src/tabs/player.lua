38c4a3299b0fedecb6e4642eedddc6582a1d6934
mrxzhli-ship-it <mrxzhli@gmail.com>
2026-03-30 18:20:39 +0800

    Update player.lua

--- src/tabs/player.lua
+++ src/tabs/player.lua
@@ -1,1 +1,17 @@
+return function(tab)
+    tab:CreateToggle({
+        Name = "Godmode",
+        CurrentValue = false,
+        Flag = "GodmodeToggle",
+        Callback = function(value)
+            print("Godmode:", value)
+        end
+    })
 
+    tab:CreateButton({
+        Name = "Teleport (Example)",
+        Callback = function()
+            print("Teleport triggered")
+        end
+    })
+end
